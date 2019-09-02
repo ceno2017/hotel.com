b1d71ecf094754c2329aa759a55fa221019018df
Charles Eno <mybizdesk_50@yahoo.com>
2019-09-02 04:25:41 +0100

    handle change
    handle change

--- src/context.js
+++ src/context.js
@@ -66,7 +66,7 @@ class RoomProvider extends React.Component {
   handleChange = event => {
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
+    const name = event.target.name;
     this.setState(
       {
         [name]: value
